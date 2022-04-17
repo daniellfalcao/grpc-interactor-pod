3c360359bb702e6d7040cfedfd20ff46e3a6cde0
Daniel Falcão <dev.danielfalcao@gmail.com>
2022-04-16 23:17:42 -0300

    Initial Commit

---
+++ GRPCInteractor/GRPCInteractor.h
@@ -0,0 +1,18 @@
+//
+//  GRPCInteractor.h
+//  GRPCInteractor
+//
+//  Created by Daniel on 4/16/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GRPCInteractor.
+FOUNDATION_EXPORT double GRPCInteractorVersionNumber;
+
+//! Project version string for GRPCInteractor.
+FOUNDATION_EXPORT const unsigned char GRPCInteractorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GRPCInteractor/PublicHeader.h>
+
+
